81359d44c9f5997b44ea57794ec9de7a2d9db5d2
Олег Каргаев <oleg.kargaev@gmail.com>
2023-01-27 17:50:01 +0300

    if main

--- main.py
+++ main.py
@@ -101,7 +101,7 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+if __name__ == '__main__':
     run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
